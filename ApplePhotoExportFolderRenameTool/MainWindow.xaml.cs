6f06f67f825af612797d6be0ae795490cd34cdc8
George Chang <georgec@iointerconnect.com.tw>
2018-09-03 17:01:41 +0800

    Add log ui

--- ApplePhotoExportFolderRenameTool/MainWindow.xaml.cs
+++ ApplePhotoExportFolderRenameTool/MainWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using Path = System.IO.Path;
 
@@ -23,7 +14,7 @@ namespace ApplePhotoExportFolderRenameTool
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         private string _selectedPath = string.Empty;
         public MainWindow()
@@ -59,39 +50,40 @@ namespace ApplePhotoExportFolderRenameTool
                 {
                     var parentDirectory = Directory.GetParent(directory).ToString();
                     var currentDirectoryName = Path.GetFileName(directory);
-                    var newDirectoryName = string.Empty;
+                    const string appleDefaultExportFolderPatten = @"^(?<area>.* - )?(?<street>.*, )?(?<date>.*)";
                     var rx = new Regex(
-                        @"^(?<area>.* - )?(?<street>.*, )?(?<date>.*)",
+                        appleDefaultExportFolderPatten,
                         RegexOptions.Compiled | RegexOptions.IgnoreCase);
                     Debug.Assert(currentDirectoryName != null);
-                    var matches = rx.Matches(currentDirectoryName);
-                    Debug.Assert(matches.Count == 1);
-                    var dateTime = string.Empty;
-                    foreach (Match match in matches)
+                    var matchAappleDefaultExportFolderPatten = rx.Match(currentDirectoryName);
+                    if (matchAappleDefaultExportFolderPatten.Captures.Count == 0)
                     {
-                        var groups = match.Groups;
-
-                        const string patternDateTime = @"(?<year>\d*)年(?<month>\d*)月(?<date>\d*)日";
-                        var rxDateTime = new Regex(
-                            patternDateTime,
-                            RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
-                        dateTime = groups["date"].Value;
-                        var matchDateTime = rxDateTime.Match(dateTime);
-                        if (matchDateTime.Captures.Count == 0)
-                        {
-                            Debug.WriteLine($"No match string found!!! {directories}");
-                            return;
-                        }
-
-                        var yearValue = int.Parse(matchDateTime.Groups["year"].Value);
-                        var monthValue = int.Parse(matchDateTime.Groups["month"].Value);
-                        var dateValue = int.Parse(matchDateTime.Groups["date"].Value);
-
-                        newDirectoryName = $"{yearValue:0000}-{monthValue:00}-{dateValue:00}";
+                        UiLog($"No match folder string found!!! {directories}");
+                        return;
                     }
 
-                    Debug.WriteLine($"\"{currentDirectoryName}\" found \"{dateTime}\", Rename as \"{newDirectoryName}\"");
+                    var groups = matchAappleDefaultExportFolderPatten.Groups;
+
+                    const string patternDateTime = @"(?<year>\d*)年(?<month>\d*)月(?<date>\d*)日";
+                    var rxDateTime = new Regex(
+                        patternDateTime,
+                        RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+                    var dateTime = groups["date"].Value;
+                    var matchDateTime = rxDateTime.Match(dateTime);
+                    if (matchDateTime.Captures.Count == 0)
+                    {
+                        UiLog($"No match string found!!! {directories}");
+                        return;
+                    }
+
+                    var yearValue = int.Parse(matchDateTime.Groups["year"].Value);
+                    var monthValue = int.Parse(matchDateTime.Groups["month"].Value);
+                    var dateValue = int.Parse(matchDateTime.Groups["date"].Value);
+
+                    var newDirectoryName = $"{yearValue:0000}-{monthValue:00}-{dateValue:00}";
+
+                    UiLog($"\"{currentDirectoryName}\" found \"{dateTime}\", Rename as \"{newDirectoryName}\"");
                     var newDirectory = Path.Combine(parentDirectory, newDirectoryName);
                     if (Directory.Exists(newDirectory))
                     {
@@ -109,7 +101,7 @@ namespace ApplePhotoExportFolderRenameTool
                         }
                         catch (Exception exception)
                         {
-                            Debug.WriteLine($"Exception: {exception.Message}");
+                            UiLog($"Exception: {exception.Message}");
                         }
                     }
                     else
@@ -120,11 +112,24 @@ namespace ApplePhotoExportFolderRenameTool
                         }
                         catch (Exception exception)
                         {
-                            Debug.WriteLine($"Exception: {exception.Message}");
+                            UiLog($"Exception: {exception.Message}");
                         }
                     }
                 });
             });
+        }
+
+        private void UiLog(string msg)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Background, (Action)(() =>
+            {
+                var newItem = new ListBoxItem
+                {
+                    Content = $"[{DateTime.Now}] {msg}"
+                };
+                ListBoxMessages.Items.Add(newItem);
+                ListBoxMessages.ScrollIntoView(newItem);
+            }));
         }
     }
 }
