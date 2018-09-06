073f031b24b331de44fe371dc198ea8a1b9b5003
George Chang <georgec@iointerconnect.com.tw>
2018-09-06 17:30:23 +0800

    Add correct file date time information with "Creation Date" from EXIF.

---
+++ ApplePhotoExportFolderRenameTool/ExifToolWrapper.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+
+namespace ApplePhotoExportFolderRenameTool
+{
+    public class ExifTagItem
+    {
+        public string Group;
+        public string Name;
+        public string Value;
+    }
+
+    public class ExifToolWrapper : List<ExifTagItem>
+    {
+        #region public methods
+
+        public bool CheckToolExists()
+        {
+            var toolPath = GetAppPath();
+            toolPath += "\\exiftool.exe";
+            toolPath = $"\"{toolPath}\"";
+            toolPath += " -ver";
+
+            var output = "";
+            if (!File.Exists("ExifTool.exe"))
+            {
+                return false;
+            }
+
+            try
+            {
+                output = Open(toolPath);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            // check the output
+            if (output.Length < 4)
+                return false;
+
+            // (could check version number here if you care)
+            return true;
+        }
+
+        public void Run(string filename, bool removeWhitespaceInTagNames = false)
+        {
+            // exiftool command
+            var toolPath = GetAppPath();
+            toolPath += "\\exiftool.exe";
+            toolPath = $"\"{toolPath}\"";
+            if (removeWhitespaceInTagNames)
+                toolPath += " -s";
+            toolPath += " -fast -G -t -m -q -q";
+            toolPath += $" \"{filename}\"";
+
+            var output = Open(toolPath);
+
+            // parse the output into tags
+            Clear();
+            while (output.Length > 0)
+            {
+                var epos = output.IndexOf('\r');
+
+                if (epos < 0)
+                    epos = output.Length;
+                var tmp = output.Substring(0, epos);
+                var tpos1 = tmp.IndexOf('\t');
+                var tpos2 = tmp.IndexOf('\t', tpos1 + 1);
+
+                if (tpos1 > 0 && tpos2 > 0)
+                {
+                    var taggroup = tmp.Substring(0, tpos1);
+                    ++tpos1;
+                    var tagname = tmp.Substring(tpos1, tpos2 - tpos1);
+                    ++tpos2;
+                    var tagvalue = tmp.Substring(tpos2, tmp.Length - tpos2);
+
+                    // special processing for tags with binary data 
+                    tpos1 = tagvalue.IndexOf(", use -b option to extract", StringComparison.Ordinal);
+                    if (tpos1 >= 0)
+                    {
+                        tagvalue = tagvalue.Remove(tpos1, 26);
+                    }
+
+                    var itm = new ExifTagItem
+                    {
+                        Name = tagname,
+                        Value = tagvalue,
+                        Group = taggroup
+                    };
+                    Add(itm);
+                }
+
+                // is \r followed by \n ?
+                if (epos < output.Length)
+                    epos += (output[epos + 1] == '\n') ? 2 : 1;
+                output = output.Substring(epos, output.Length - epos);
+            }
+        }
+
+        public bool HasExifData()
+        {
+            return Count > 0;
+        }
+
+        public ExifTagItem Find(string tagname)
+        {
+            return this.FirstOrDefault(x => x.Name.Equals(tagname));
+            //var qItems = from tagItem in this
+            //             where tagItem.Name == tagname
+            //             select tagItem;
+            //return qItems.First();
+        }
+
+        /// <summary>
+        /// This method saves EXIF data to an external file ([file].exif). Only tags with group EXIF are saved.
+        /// </summary>
+        /// <param name="sourceImage">Source Image file path</param>
+        /// <param name="destinationExifFile">Destination .exif file path</param>
+        /// <returns>True if no error</returns>
+        public bool SaveExifData(string sourceImage, string destinationExifFile)
+        {
+            // exiftool command
+            var toolPath = GetAppPath();
+            toolPath += "\\exiftool.exe";
+            toolPath = $"\"{toolPath}\"";
+            toolPath += " -fast -m -q -q";
+            toolPath += $" -tagsfromfile \"{sourceImage}\"";
+            toolPath += $" -exif \"{destinationExifFile}\"";
+
+            var output = Open(toolPath);
+
+            if (output.Contains("Error"))
+                return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// This method writes EXIF data to the given destination image file (must exist beforehand).
+        /// </summary>
+        /// <param name="sourceExifFile">Source .exif file</param>
+        /// <param name="destinationImage">Destination image path (file must exist)</param>
+        /// <returns></returns>
+        public bool WriteExifData(string sourceExifFile, string destinationImage)
+        {
+            // exiftool command
+            var toolPath = GetAppPath();
+            toolPath += "\\exiftool.exe";
+            toolPath = $"\"{toolPath}\"";
+            toolPath += " -fast -m -q -q";
+            toolPath += $" -TagsFromFile \"{sourceExifFile}\"";
+            toolPath += $" -all:all \"{destinationImage}\"";
+
+            var output = Open(toolPath);
+
+            if (output.Contains("Error"))
+                return false;
+
+            return true;
+        }
+        #endregion
+        #region Private methods
+        /// <summary>
+        /// Gets the path from where the executable is being run
+        /// </summary>
+        /// <returns>Path</returns> 
+        private string GetAppPath()
+        {
+            var appPath = Assembly.GetExecutingAssembly().Location;
+            appPath = Path.GetDirectoryName(appPath);
+
+            return appPath;
+        }
+
+        private string _stdOut;
+        private string _stdErr;
+        private ProcessStartInfo _psi;
+        private Process _activeProcess;
+
+        private void Thread_ReadStandardError()
+        {
+            if (_activeProcess != null)
+            {
+                _stdErr = _activeProcess.StandardError.ReadToEnd();
+            }
+        }
+
+        private void Thread_ReadStandardOut()
+        {
+            if (_activeProcess != null)
+            {
+                _stdOut = _activeProcess.StandardOutput.ReadToEnd();
+            }
+        }
+
+        private string Open(string cmd)
+        {
+            var program = "\"%COMSPEC%\"";
+            var args = "/c \"[command]\"";
+            _psi = new ProcessStartInfo(
+                Environment.ExpandEnvironmentVariables(program),
+                args.Replace("[command]", cmd)
+            )
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            var threadReadStandardError = new Thread(Thread_ReadStandardError);
+            var threadReadStandardOut = new Thread(Thread_ReadStandardOut);
+
+            _activeProcess = Process.Start(_psi);
+            if (_psi.RedirectStandardError)
+            {
+                threadReadStandardError.Start();
+            }
+            if (_psi.RedirectStandardOutput)
+            {
+                threadReadStandardOut.Start();
+            }
+            _activeProcess?.WaitForExit();
+
+            threadReadStandardError.Join();
+            threadReadStandardOut.Join();
+
+            var output = _stdOut + _stdErr;
+
+            return output;
+        }
+
+        #endregion
+    }
+}
--- ApplePhotoExportFolderRenameTool/MainWindow.xaml.cs
+++ ApplePhotoExportFolderRenameTool/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
@@ -44,11 +45,14 @@ namespace ApplePhotoExportFolderRenameTool
         {
             Task.Run(() =>
             {
+                // 1. List all subdirectories under the target directory
                 var directories = Directory.GetDirectories(_selectedPath);
 
                 Parallel.ForEach(directories, directory =>
                 {
                     var parentDirectory = Directory.GetParent(directory).ToString();
+
+                    // 2. Separate the date part using a regular expression for each subdirectory name
                     var currentDirectoryName = Path.GetFileName(directory);
                     const string appleDefaultExportFolderPatten = @"^(?<area>.* - )?(?<street>.*, )?(?<date>.*)";
                     var rx = new Regex(
@@ -63,7 +67,7 @@ namespace ApplePhotoExportFolderRenameTool
                     }
 
                     var groups = matchAappleDefaultExportFolderPatten.Groups;
-
+                    // 3. For the date part, use the regular expression to separate the year, month, and day, and finally use the YYYY-MM-DD format to replace the original date format
                     const string patternDateTime = @"(?<year>\d*)年(?<month>\d*)月(?<date>\d*)日";
                     var rxDateTime = new Regex(
                         patternDateTime,
@@ -84,6 +88,8 @@ namespace ApplePhotoExportFolderRenameTool
                     var newDirectoryName = $"{yearValue:0000}-{monthValue:00}-{dateValue:00}";
 
                     UiLog($"\"{currentDirectoryName}\" found \"{dateTime}\", Rename as \"{newDirectoryName}\"");
+
+                    // 4. Move the data to the directory after the name is modified
                     var newDirectory = Path.Combine(parentDirectory, newDirectoryName);
                     if (Directory.Exists(newDirectory))
                     {
@@ -115,6 +121,33 @@ namespace ApplePhotoExportFolderRenameTool
                             UiLog($"Exception: {exception.Message}");
                         }
                     }
+
+                    var exifToolWrapper = new ExifToolWrapper();
+                    if (exifToolWrapper.CheckToolExists())
+                    {
+                        // 5. if we have third party tool, we can using it to help modify the file creation time to match the exif information.
+                        var fileEntries = Directory.GetFiles(newDirectory);
+
+                        foreach (var fileEntry in fileEntries)
+                        {
+                            exifToolWrapper.Run(fileEntry);
+
+                            if (exifToolWrapper.HasExifData())
+                            {
+                                var target = exifToolWrapper.Find("Create Date");
+
+                                if (target != null)
+                                {
+                                    var createDate = DateTime.ParseExact(target.Value, "yyyy:MM:dd HH:mm:ss.FFF", null);
+                                    File.SetCreationTime(fileEntry, createDate);
+                                    File.SetLastAccessTime(fileEntry, DateTime.Now);
+                                    File.SetLastWriteTime(fileEntry, createDate);
+
+                                    // TODO: Double check the file in the correct date folder, assume Photos app export correctly.
+                                }
+                            }
+                        }
+                    }
                 });
             });
         }
@@ -130,6 +163,53 @@ namespace ApplePhotoExportFolderRenameTool
                 ListBoxMessages.Items.Add(newItem);
                 ListBoxMessages.ScrollIntoView(newItem);
             }));
+        }
+
+        private void BtnCorrectDate_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                var directories = Directory.GetDirectories(_selectedPath);
+
+                Parallel.ForEach(directories, directory =>
+                {
+                    try
+                    {
+                        var exifToolWrapper = new ExifToolWrapper();
+                        if (exifToolWrapper.CheckToolExists())
+                        {
+                            UiLog($"Begin process files in {Path.GetFileName(directory)}");
+                            // 5. if we have third party tool, we can using it to help modify the file creation time to match the exif information.
+                            var fileEntries = Directory.GetFiles(directory);
+
+                            foreach (var fileEntry in fileEntries)
+                            {
+                                exifToolWrapper.Run(fileEntry);
+
+                                if (exifToolWrapper.HasExifData())
+                                {
+                                    var target = exifToolWrapper.Find("Create Date");
+
+                                    if (target != null)
+                                    {
+                                        var createDate = DateTime.ParseExact(target.Value, "yyyy:MM:dd HH:mm:ss.FFF", null);
+                                        File.SetCreationTime(fileEntry, createDate);
+                                        File.SetLastAccessTime(fileEntry, DateTime.Now);
+                                        File.SetLastWriteTime(fileEntry, createDate);
+
+                                        // TODO: Double check the file in the correct date folder, assume Photos app export correctly.
+                                    }
+                                }
+                            }
+                            UiLog($"Finished {fileEntries.Length} files in {Path.GetFileName(directory)}");
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        UiLog($"Exception: \"{exception.Message}\" on \"{directory}\"");
+                    }
+                });
+            });
         }
     }
 }
